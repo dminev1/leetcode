2c9fe70cdca94fbe11188b30793139d38d90760a
Dimitar Minev <37195786+dminev1@users.noreply.github.com>
2026-01-19 21:03:48 -0500

    Time: 3 ms (74.30%) | Memory: 46.5 MB (55.97%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        if(s.length()==1){return getInteger(s.charAt(0));}
+        for(int i=1; i < s.length(); i++){
+            if(getInteger(s.charAt(i)) > getInteger(s.charAt(i-1))){
+                result += getInteger(s.charAt(i)) - getInteger(s.charAt(i-1));
+                i++;
+            }else {
+                result += getInteger(s.charAt(i-1));
+            }
+            if(i == s.length()-1 && getInteger(s.charAt(i)) <= getInteger(s.charAt(i-1))){
+                result += getInteger(s.charAt(i));
+            }
+        }
+        return result;
+    }
+
+    public int getInteger (char c){
+        switch(c)
+        {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return -1;
+        }
+    }
+}
